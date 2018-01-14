1e6fb85090fb799df2e45d17959363f92507fce1
Eric Chen <cyh05040@gmail.com>
2018-01-14 17:43:16 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,6 +1,10 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
 }
+cars = [audi, mercedes, bmw, toyota]
+cars.push = [honda]
+
+
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
